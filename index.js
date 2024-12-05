51a0c6cdc80e49315453800c4d71d2b6d0616035
Kelvin Muchiri <muchirikelv71@gmail.com>
2024-12-05 14:41:57 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+//arrow function code
+
+//declare function expression called divide
+const divide = function() {
+    return 2000 / 100;
+}
+
+//declare an arrow function called square
+const square = (k) => k * k;
+
+//declare an arrow function called add
+const add = (a, b) => a + b; 
+
+
+console.log(divide());     //output 200
+console.log(square(8));    //output 64
+console.log(add(30, 20));  //output 50
